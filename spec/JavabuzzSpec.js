ddccdd9db65c6589c8467a64604f165907e54b8e
Scully87 <scully87.ts@googlemail.com>
2014-10-14 11:56:19 +0100

    jasmine passes knows when a number is/not divisible by 3/5/15

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -12,6 +12,30 @@ describe('Javabuzz', function() {
 			expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
 		});
 
+		it('divisible by 5', function() {
+			expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
+		});
+
+		it('divisible by 15', function() {
+			expect(javabuzz.isDivisibleByFifteen(15)).toEqual(true);
+		});
+
+	});
+
+	describe('knows when a number is NOT',function() {
+
+		it('divisible by 3', function() {
+			expect(javabuzz.isDivisibleByThree(1)).toEqual(false);
+		});
+
+		it('divisible by 5', function() {
+			expect(javabuzz.isDivisibleByFive(1)).toEqual(false);
+		});
+
+		it('divisible by 15', function() {
+			expect(javabuzz.isDivisibleByFifteen(1)).toEqual(false);
+		});
+
 	});
 
 });
