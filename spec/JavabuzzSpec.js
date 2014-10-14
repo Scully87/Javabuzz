b4bcc358e33ba12282a5400463c5fa1bd4f2c7b2
Scully87 <scully87.ts@googlemail.com>
2014-10-14 12:44:10 +0100

    javabuzz jasmine tests passing, javabuzz returns results, played with in console

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -6,13 +6,14 @@ describe('Javabuzz', function() {
 		javabuzz = new Javabuzz();
 	});
 
-	describe('knows when a number is',function() {
+	describe('knows when a number is', function() {
 
 		it('divisible by 3', function() {
 			expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
 		});
 
 		it('divisible by 5', function() {
+
 			expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
 		});
 
@@ -34,6 +35,23 @@ describe('Javabuzz', function() {
 
 		it('divisible by 15', function() {
 			expect(javabuzz.isDivisibleByFifteen(1)).toEqual(false);
+		});
+
+	});
+
+	describe('when playing, says', function() {
+		
+
+		it('"Java" when a number is divisible by 3', function() {
+		  expect(javabuzz.says(3)).toEqual("Java");
+		});
+				
+		it('"Buzz" when a number is divisible by 5', function() {
+		  expect(javabuzz.says(5)).toEqual("Buzz");
+		});
+
+		it('"JavaBuzz" when a number is divisible by 15', function() {
+		  expect(javabuzz.says(15)).toEqual("Javabuzz");
 		});
 
 	});
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,13 +1,25 @@
 function Javabuzz() {};
 
-	Javabuzz.prototype.isDivisibleByThree = function(number) {
-		return (number % 3 === 0);
+	Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+		return (number % 15 === 0);
 	};
 
 	Javabuzz.prototype.isDivisibleByFive = function(number) {
 		return (number % 5 === 0);
 	};
 
-	Javabuzz.prototype.isDivisibleByFifteen = function(number) {
-		return (number % 15 === 0);
+	Javabuzz.prototype.isDivisibleByThree = function(number) {
+		return (number % 3 === 0);
 	};
+
+	Javabuzz.prototype.says = function(number) {
+		if (this.isDivisibleByFifteen(number)) return "Javabuzz";
+		if (this.isDivisibleByThree(number)) return "Java";
+		if (this.isDivisibleByFive(number)) return "Buzz";
+		return number;
+	};
+
+	
+
+
+	
